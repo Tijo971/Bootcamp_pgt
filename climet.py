3c88e688ad6ba71f38313492266488d5c1b2ff2e
Tijo971 <84888671+Tijo971@users.noreply.github.com>
2021-06-22 21:47:42 +0530

    Add files via upload

---
+++ climet.py
@@ -0,0 +1,33 @@
+import requests
+import json
+#import os
+from datetime import datetime
+
+api_key = '856360030c37eb7a391023b0410cc67d'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+clymet = {
+"temp_city" : ((api_data['main']['temp']) - 273.15),
+"weather_desc" : api_data['weather'][0]['description'],
+"hmdt" : api_data['main']['humidity'],
+"wind_spd" : api_data['wind']['speed'],
+"date_time" : datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+}
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(),clymet ["date_time"]))
+print ("-------------------------------------------------------------")
+
+print ("Current Humidity      :",clymet ["hmdt"], '%')
+print ("Current temperature is: {:.2f} deg C".format(clymet["temp_city"]))
+print ("Current wind speed    :",clymet["wind_spd"] ,'kmph')
+print ("Current weather desc  :",clymet["weather_desc"])
+
+json_obj=json.dumps(clymet,indent=2)
+with open("climet.txt","w") as g:
+         g.write("Weather Status for :--"+location.upper()+"\n"+json_obj)
+
